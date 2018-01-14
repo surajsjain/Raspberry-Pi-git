0b21b7bf71b22c218a3d39376ff52955d4a4a327
Suraj S Jain <surajsjain@hotmail.com>
2018-01-14 12:06:13 +0530

    LED off solved
    LED wasn't  turning off. so, I put another sleep for 1 second after outputting Low to GPIO 18

--- ledexample.py
+++ ledexample.py
@@ -1,13 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
+//setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 
-
+//code
 for i in range(1,10):
     GPIO.output(18, GPIO.HIGH)
-    time.sleep(5)
+    time.sleep(1)
     GPIO.output(18, GPIO.LOW)
+    time.sleep(1)
 
 GPIO.cleanup()
