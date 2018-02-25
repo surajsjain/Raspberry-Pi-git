f7f47306f9b448bfbdf523fa29f8e1476677afe1
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 20:07:35 +0530

    00

--- servo_test.py
+++ servo_test.py
@@ -3,23 +3,10 @@ import RPi.GPIO as gpio
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(7,gpio.OUT)
-gpio.output(7, True)
-time.sleep(0.0015)
-gpio.output(7, False)
-'''
-pwm = gpio.PWM(7,100)
-pwm.start(2.5)
-def change(k):
-    pwm.ChangeDutyCycle(k)
-    time.sleep(1)
-
-try:
-    while True:
-        k = float(raw_input("enter angle: "))
-        k = (1+(float(k)/180))/20
-        change(k)
-except KeyboardInterrupt:
-    pwm.stop()
-    gpio.cleanup()
-    exit
-'''
+pwm=gpio.pwm(7, 50)
+pwm.start(5)
+for x in range (0,7):
+    pwm.ChangeDutyCycle(x)
+    time.sleep(0.5)
+GPIO.cleanup()
+pwm.stop
