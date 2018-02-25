1eb0cc36b6818b7ca53ca58182661b405b968b72
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 20:11:52 +0530

    00

--- servo_test.py
+++ servo_test.py
@@ -1,12 +1,8 @@
+import RPi.GPIO as GPIO
 import time
-import RPi.GPIO as gpio
-gpio.setwarnings(False)
-gpio.setmode(gpio.BOARD)
-gpio.setup(7,gpio.OUT)
-pwm=gpio.PWM(7, 50)
-pwm.start(5)
-for x in range (0,7):
-    pwm.ChangeDutyCycle(x)
-    time.sleep(0.5)
-gpio.cleanup()
-pwm.stop
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(7, GPIO.OUT)
+servo=GPIO.PWM(7, 50)
+servo.start(0)
