f626ec58554b6d172f3b8cb2ff4742cb086a8f9d
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 20:08:22 +0530

    00

--- servo_test.py
+++ servo_test.py
@@ -3,7 +3,7 @@ import RPi.GPIO as gpio
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(7,gpio.OUT)
-pwm=gpio.pwm(7, 50)
+pwm=gpio.PWM(7, 50)
 pwm.start(5)
 for x in range (0,7):
     pwm.ChangeDutyCycle(x)
