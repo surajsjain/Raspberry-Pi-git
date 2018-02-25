0c38978c98343c9a9d546934f11081b8e340e925
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 19:52:42 +0530

    Change of plans

--- servo_test.py
+++ servo_test.py
@@ -1,8 +1,20 @@
-import RPi.GPIO as GPIO
 import time
+import RPi.GPIO as gpio
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(7,gpio.OUT)
+pwm = gpio.PWM(7,100)
+pwm.start(2.5)
+def change(k):
+    pwm.ChangeDutyCycle(k)
+    time.sleep(1)
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(07, GPIO.OUT)
-servo=GPIO.PWM(07, 50)
-servo.ChangeDutyCycle(50)
+try:
+    while True:
+        k = float(raw_input("enter angle: "))
+        k = (1+(float(k)/180))/20
+        change(k)
+except KeyboardInterrupt:
+    pwm.stop()
+    gpio.cleanup()
+    exit
