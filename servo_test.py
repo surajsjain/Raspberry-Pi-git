920bea6feaaef82a5f9188b0bcd2298182fd8783
Suraj S Jain <surajsjain@hotmail.com>
2018-02-25 19:34:58 +0530

    more to servo program

--- servo_test.py
+++ servo_test.py
@@ -3,6 +3,16 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(7, GPIO.OUT)
-servo=GPIO.PWM(7, 50)
+GPIO.setup(07, GPIO.OUT)
+servo=GPIO.PWM(07, 50)
 servo.start(2.0)
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+	GPIO.output(07, True)
+	servo.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(07, False)
+	pwm.ChangeDutyCycle(0)
+
+SetAngle(90)
